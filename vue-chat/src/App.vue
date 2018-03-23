f1aef4063dc3efdc080366333b6bf0b210382f91
yangyangshasha <yangyangshasah@outlook.com>
2018-03-23 23:20:11 +0800

    done card list

---
+++ vue-chat/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+<div id="app">
+    <div class="sidebar">
+        <card></card>
+        <list></list>
+    </div>
+    <div class="main">
+
+    </div>
+</div>
+</template>
+
+<script>
+import card from './components/card';
+import list from './components/list';
+
+export default {
+  name: 'App',
+  components: {
+    card,
+    list
+  }
+}
+</script>
+
+<style scoped>
+    #app {
+        margin: 20px auto;
+        width: 800px;
+        height: 600px;
+        overflow: hidden;
+        border-radius: 3px;
+    }
+    .sidebar, .main {
+        height: 100%;
+    }
+    .sidebar {
+        float: left;
+        width: 200px;
+        color: #f4f4f4;
+        background-color: #2e3238;
+    }
+    .main {
+        position: relative;
+        overflow: hidden;
+        background-color: #eee;
+    }
+    .text {
+        position: absolute;
+        width: 100%;
+        bottom: 0;
+        left: 0;
+    }
+    .message {
+        height: calc(100% - 160px);
+    }
+</style>
---
+++ vue-chat/src/components/card.vue
@@ -0,0 +1,57 @@
+<template>
+<div class="card">
+  <header>
+      <!-- <img class="avatar" :src="$store.state.user.img" :alt="$store.state.user.img"> -->
+      <!-- <p class="name">{{$store.state.user.name}}</p> -->
+      <img src="./../assets/logo.png" alt="" class="avatar">
+      <p class="name">youngyang</p>
+  </header>
+  <footer>
+      <input class="search" type="text" placeholder="search user...">
+  </footer>
+</div>
+</template>
+<script>
+// import srore from '../store';
+
+// export default {
+  
+// }
+</script>
+<style scoped>
+    .card {
+        padding: 12px;
+        border-bottom: solid 1px #24272C;
+    }
+    header > .avatar{
+        width: 40px;
+        height: 40px;
+    }
+    footer {
+        margin-top: 10px;
+    }
+    .avatar, .name {
+        vertical-align: middle;
+    }
+    .avatar {
+        border-radius: 2px;
+    }
+    .name {
+        display: inline-block;
+        margin: 0 0 0 15px;
+        font-size: 16px;
+    }
+    .search {
+        width: 150px;
+        padding: 0 10px;
+        width: 100%;
+        font-size: 12px;
+        color: #fff;
+        height: 30px;
+        line-height: 30px;
+        border: solid 1px #3a3a3a;
+        border-radius: 4px;
+        outline: none;
+        background-color: #26292E;
+    }
+</style>
